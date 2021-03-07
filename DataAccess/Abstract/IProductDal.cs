e25fe9b7e60d262009b2ed775431427504188331
büşra adlığ <adlibusra@gmail.com>
2021-03-07 21:11:28 +0300

    DersSonu

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using Core.DataAccess;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,6 +11,8 @@ namespace DataAccess.Abstract
     //Add REference to entities // Entities katmanını kullanıcam ampülle onu seçtim
     public interface IProductDal: IEntityRepository<Product>
     {
+
+        List<ProductDetailDto> GetProductDetailDtos();
         // interdface metotları default publictir
         
 
--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,6 +1,7 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,8 +13,24 @@ namespace DataAccess.Concrete.EntityFramework
 {
 
     //NuGet : Entity Framwork pakaet olarak geliyor 
-   public class EfProductDal : EfEntityRepositoryBase<Product,NorthWindContext>,IProductDal
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthWindContext>, IProductDal
     {
-        
+        public List<ProductDetailDto> GetProductDetailDtos()
+        {
+            using (NorthWindContext context = new NorthWindContext())
+            {
+                var result = from p in context.Products
+                             join c in context.Categories
+                             on p.CategoryID equals c.CategoryId
+                             select new ProductDetailDto
+                             {
+                                 ProductID = p.ProductID,
+                                 ProductName = p.ProductName,
+                                 CategoryName = c.CategoryName,
+                                 UnitsInStock = p.UnitsInStock
+                             };
+                return result.ToList();
+            }
+        }
     }
 }
